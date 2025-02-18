94cc8a23cbf8874df88eeb6652d2e59b16869c23
mc4 <mark.e.conley@gmail.com>
2025-02-18 12:30:20 -0500

    code clean up

--- src/test/java/com/wakefernfoodcorp/LinkedListTest.java
+++ src/test/java/com/wakefernfoodcorp/LinkedListTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class LinkedListTest {
+public class LinkedListTest {
 
 	private LinkedList<Integer> list;
 
